a36f3a1fb05a291a183ac0da02256d5b296fb19b
smendez <the_true_spider@yahoo.com>
2023-12-01 22:28:36 -0600

    restore react to base

--- first_project/theblog/urls.py
+++ first_project/theblog/urls.py
@@ -6,5 +6,5 @@ from . views import HomeView, SlideShowView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('slideshow/', SlideShowView.as_view(), name='slideshow'),
+    path('slideshow/', SlideShowView.as_view(), name='index.html'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
